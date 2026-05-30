572d35be9f91c10df616aa61010213964b711d56
dev <tonyringertx@gmail.com>
2026-05-29 20:48:32 -0500

    Convert MyFirstProject to standard Maven/Spring Boot project structure

---
+++ MyFirstProject/src/main/java/com/thedevdepot/myfirstproject/MyFirstProjectApplication.java
@@ -0,0 +1,11 @@
+package com.thedevdepot.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstProjectApplication.class, args);
+    }
+}
---
+++ MyFirstProject/src/test/java/com/thedevdepot/myfirstproject/MyFirstProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.thedevdepot.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
