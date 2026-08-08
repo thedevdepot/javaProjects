e060a7edf54e615df5366bf5397eaf1d64841ad5
The Dev Depot <tonyringertx@gmail.com>
2026-08-08 19:39:37 +0000

    input two sum code for practice

--- javaPractice/main.java
+++ javaPractice/main.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.HashMap;
+import java..util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap <> ();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[] { numToIndex.get(complement), i };
+            }
+
+            // Add current number and index to map
+            numToIndex.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No solution for two sum");
+    }
+}
+
+/* public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
+*/
